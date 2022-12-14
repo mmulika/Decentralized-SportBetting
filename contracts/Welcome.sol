7e31eb03e5f5151021d5807ba6312938aa873d8d
mmulika <mulikamike@gmail.com>
2022-12-14 18:08:29 +0300

    welcom-contract

---
+++ contracts/Welcome.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+contract Welcome{
+
+    //functions
+    uint x;
+    event getValue;
+    function get( uint x){
+        emit getValue()
+    
+    }
+
+    function setValue(uint ){
+        var uint  x
+        x = x
+        console.log(x)
+
+    }
+}
